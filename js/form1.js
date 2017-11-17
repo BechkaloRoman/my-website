deb94c71de9294369536bb28b6a4b327649b725b
Bechkalo Roman <bechkalor@gmail.com>
2017-11-17 16:01:51 +0000

    Changet15

--- js/form1.js
+++ js/form1.js
@@ -17,7 +17,7 @@ function sendForm(e) {
       
     });
 }
-   */
+   *//*
    $(function(){
     $('form input[type=submit]').click(function(e){
         if(!$('form')[0].checkValidity()){
@@ -28,7 +28,7 @@ function sendForm(e) {
             url: "https://formspree.io/bechkalor@gmail.com",
             method: "POST",
             data: { 
-               name: $('#name').val(),
+                name: $('#name').val(),
                 email: $('#email').val(),
                 message: $('#message').val()
                 
@@ -43,4 +43,30 @@ function sendForm(e) {
     })
     
     
-})
+})*/
+$('form input[type="submit"]').click(sendForm);
+
+function sendForm(e) {
+    const form = $('form')[0];
+    if (!form.checkValidity()) {
+        return;
+    }
+    e.preventDefault();
+    $.ajax({
+        url: "https://formspree.io/bechkalor@gmail.com", 
+        method: "POST",
+        data: {
+           name: $('#name').val(),
+           email: $('#email').val(),
+           message: $('#message').val()
+        },
+        dataType: "json"
+    })
+    .done(function(){
+        $('form')[0].reset();
+        $('#msg').html('Thank you!');
+    })
+    .fail(function(){
+        $('#msg').html('Sorry, there is an error!');
+    });
+}
