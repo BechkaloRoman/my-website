0025ec533f30a39b198c2a958a30e4e3a4143854
Bechkalo Roman <bechkalor@gmail.com>
2017-11-20 14:40:21 +0000

    Changet02

--- js/smooth-skroll.js
+++ js/smooth-skroll.js
@@ -5,7 +5,7 @@ $(function() {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
-            }, 2500);
+            }, 1500);
         }
     });
 });
